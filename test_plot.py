3ddf935b520ee80901a060b4ecfa402148bbbe24
rmp <rmp@ruimp.com>
2020-07-11 11:48:15 +0100

    tests

---
+++ test_plot.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import trf_matrix as trf
+import numpy as np
+import matplotlib.pyplot as plt
+
+n_in = 1.46
+n_pol = 1.6
+n_out = 1.0
+xi = 0.149
+n_layer = np.ones(99) * n_pol
+d = 10
+d_list = [5, 10, 15, 20]
+wl_list = [600, 800, 1000, 1200]
+
+T = trf.TransferMatrix(300, -0.2, wl_list)
+
+T.get_constant_xi(xi)
+T.plot_field_vs_d(n_in, n_layer, n_out, d_list, wl_ind = 2)
+#T.plot_r_vs_wl(n_in, n_layer, n_out, d_list)
+plt.show()
