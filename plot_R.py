4aca881a65a9436807a02396e3a17981a86d822e
ruimp <rmp@ruimp.com>
2020-09-22 10:21:54 +0100

    PLOTS

--- plot_R.py
+++ plot_R.py
@@ -2,36 +2,36 @@
 
 import trf_matrix as trf
 import numpy as np
-import matplotlib.pyplot as plt
+import os
+os.chdir("/home/rmp/Projects/TransferMatrix/Nlayers")
 
 n_in = 1.46
 n_pol = 1.6
-n_out_list = [1, 1.4, 1.8, 2.2, 2.6, 3]
-n_layers = 40
-n_pol_list = np.ones(n_layers - 1) * n_pol
-d = 10
-wl_list = np.arange(600, 2600, 200)
-
+n_out_list = np.arange(1, 2, .2)
+n_layers_list = np.arange(15, 50, 5)
+ds = np.arange(5, 45, 5)
+wl_list = np.arange(400, 1400, 200)
 T = trf.TransferMatrix(300, -0.2, wl_list)
 T.get_graphene_xi()
 
-fig, axs = plt.subplots(3, 2, sharex=True, sharey=True, gridspec_kw={'hspace':0.8, 'wspace':0.03})
-for i, n_out in enumerate(n_out_list):
-    for j, wl in enumerate(wl_list):
-        R = np.zeros(n_layers-1)
-        for n in range(1, n_layers):
-            T.trf_routine(n_in, n_pol_list[:n], n_out, d, j)
-            T.get_R()
-            R[n-1] = T.R*100
-        axs[i//2, i%2].plot(np.arange(2, n_layers + 1, dtype=int), R, label=r"$\lambda$={}".format(wl))
-        if i == 0:
-            axs[i//2, i%2].legend()
-
-plt.xticks(ticks=np.arange(2, n_layers + 1, 4, dtype=int))
-fig.add_subplot(111, frameon=False)
-plt.tick_params(labelcolor="none", top=False, bottom=False, left=False, right=False)
-plt.xlabel("Number of layers")
-plt.ylabel("Reflectance (%)", labelpad=10)
-plt.title(r"$\lambda \in$ [600, 2600, 200] | n $\in$ [1, 3, .4]")
-plt.savefig("Week2/Plot_R_vs_40Layers.pdf", dpi=800, bbox_inches="tight")
-plt.show()
+for n_layers in n_layers_list:
+    n_pol_list = np.ones(n_layers - 1) * n_pol
+    for d in ds:
+        d_list = np.ones(n_layers) * d
+        for j in range(T.n_wls):
+            for i, n_out in enumerate(n_out_list):
+                data = np.zeros((n_layers, 2))
+                R = np.zeros(n_layers)
+                T.init_trf_matrix()
+                T.multiply_by_layer(n_in, n_out, j)
+                T.get_R()
+                data[0, 0] = 1
+                data[0, 1] = T.R
+                for n in range(1, n_layers):
+                    T.trf_routine(n_in, n_pol_list[:n], n_out, d_list[:n+1], j)
+                    T.get_R()
+                    R[n] = T.R*100
+                    data[n, 0] = n+1
+                    data[n, 1] = T.R
+                filename = "N{}:ni{:.2f}:np{:.2f}:nf{:.2f}:d{:.2f}:wl{}.dat".format(n_layers, n_in, n_pol, n_out, d, wl_list[j])
+                np.savetxt(filename, data, delimiter="\t")
